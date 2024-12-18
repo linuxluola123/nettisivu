816326c6cd8efece99ec90c847dafbbc6aafe4b3
linuxluola123 <pelllaaaja@outlook.com>
2024-12-18 21:05:54 +0200

    Add files via upload

---
+++ scripti.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+echo "Tämä ohjelma asentaa openttdn koodísta debianille"
+cd ~/
+sudo apt install build-essential pkg-config libsdl1.2-dev git zlib1g-dev liblzo2-dev liblzma-dev libfontconfig-dev libicu-dev libfluidsynth-dev
+git clone https://github.com/OpenTTD/OpenTTD.git .openttdkoodi
+cd .openttdkoodi
+mkdir build
+cd build
+cmake ..
+make
+cd ~/
+mv alias.txt alias_tempbackup.txt
+wget https://linuxluola123.github.io/nettisivu/alias.txt
+cat alias.txt >> .bashrc
+rm alias.txt
+mv alias_tempbackup.txt alias.txt
+notify-send "openttd on asennettu""
